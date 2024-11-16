bbb5fb9350c0470733ced7db206aa193169e47f7
Pavel Artiev <arijev@bk.ru>
2024-11-17 00:03:56 +0300

    zadacha 3

--- src/taxes.py
+++ src/taxes.py
@@ -13,3 +13,28 @@ def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
         taxed_prices.append(price + tax)
 
     return taxed_prices
+
+
+def calculate_tax(price: float,
+                  tax_rate: float,
+                  *,
+                  discount: float = 0,
+                  round_digits: int = 2
+) -> float:
+
+    for arg in [price, tax_rate, round_digits, discount]:
+        if not isinstance(arg, (int | float)):
+            raise TypeError("Ошибка типа данных")
+
+    if price < 0:
+        raise ValueError('Неверная цена')
+
+    if tax_rate < 0:
+        raise ValueError('Неверный налоговый процент')
+
+    if tax_rate >= 100:
+        raise ValueError('Неверный налоговый процент')
+
+    new_price = price + price * tax_rate / 100
+    price_with_discount = new_price - new_price * discount / 100
+    return round(price_with_discount, round_digits)
